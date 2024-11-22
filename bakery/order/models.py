ed87462b99c9bdcc9b9be20328533957479d3657
Edvard <edvard99_00@mail.ru>
2024-11-22 14:10:11 +0300

    1) Создание моделей для водителей, клиентов и продуктов. Клиенты связаны с водителями(Каждый клиент связан с одним из водителей) 2) Создание файла add_data.html, содержащего форму для добавления данных в вышеуказанные таблицы

--- bakery/order/models.py
+++ bakery/order/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Drivers(models.Model):
+    driver = models.CharField(max_length=50, unique=True, null=False)
+
+
+class Clients(models.Model):
+    client = models.CharField(max_length=50, unique=True, null=False)
+    driver_id = models.ForeignKey('Drivers', on_delete=models.PROTECT)
+
+
+class Products(models.Model):
+    product = models.CharField(max_length=50, unique=True, null=False)
--- bakery/order/views.py
+++ bakery/order/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+def add_data(request):
+    return render(request, 'order/add_data.html')
