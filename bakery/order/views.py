16307c8c47aa6baccf09d6c8d4fb4117498618cc
Edvard <edvard99_00@mail.ru>
2024-11-22 14:33:05 +0300

    Добавление в фале add_data.html тега select, отображающего имеющихся водителей, при добавлении нового клиента Добавление функционала в функции отображения для добавления новых записей в таблицах

--- bakery/order/views.py
+++ bakery/order/views.py
@@ -1,6 +1,21 @@
 from django.shortcuts import render
 
+from order.models import Drivers, Clients, Products
+
+
 # Create your views here.
 
 def add_data(request):
-    return render(request, 'order/add_data.html')
+    if request.method == 'POST':
+        if 'driver' in request.POST and request.POST['driver'] != '':
+            Drivers.objects.create(driver=request.POST['driver'])
+
+        if 'client' in request.POST and 'drive' in request.POST and request.POST['client'] != '':
+            drive = Drivers.objects.filter(driver=request.POST['drive'])[0]
+            Clients.objects.create(client=request.POST['client'], driver_id=drive)
+
+        if 'product' in request.POST and request.POST['product'] != '':
+            Products.objects.create(product=request.POST['product'])
+
+    drivers = Drivers.objects.all()
+    return render(request, 'order/add_data.html', {'drivers': drivers})
